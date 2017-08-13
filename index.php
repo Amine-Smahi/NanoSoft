98271b3324a15df769f29d436897f421279e5645
Amine Smahi <mohammed.amine.smahi@gmail.com>
2017-08-13 01:15:37 +0100

    add rtl support and droid kuffy font

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<html>
+<html dir="rtl" lang="ar">
 <head>
 	<title></title>
 
@@ -6,13 +6,17 @@
 	<link rel="stylesheet" type="text/css" href="css/global.css">
 	<!--Bootstrap CSS-->
 	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
-	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
-
+	
     <link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
+    <link href="http://fonts.googleapis.com/earlyaccess/droidarabickufi.css" rel="stylesheet">
     <!--Script-->
     <script src="js/jquery.js"></script>
     <script src="js/bootstrap.js"></script>
-    <script src="js/bootstrap.min.js"></script>
+   <style>
+       body{
+           font-family: 'Droid Arabic Kufi', sans-serif;
+       }
+    </style>
 </head>
 <body>
 	<!-- Navigation -->
@@ -30,7 +34,14 @@
                 <a class="navbar-brand page-scroll" href="home.php"></a>
             </div>
             <div class="navbar-header">
-                <a class="navbar-brand" href="home.php">NanoSoft</a>
+                <form class="navbar-form" method="POST"role="search" action="pages/login.php">
+					<div class="form-group">
+					<input type="text" class="form-control" name="username"placeholder="Username">
+					<input type="password" class="form-control" name="password"placeholder="Password">
+					</div>
+					<button type="submit" class="btn btn-success">Login</button>
+					</form>
+               
             </div>
             <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
@@ -40,13 +51,7 @@
                 </ul>
             -->
                 <div>
-					<form class="navbar-form navbar-right" method="POST"role="search" action="pages/login.php">
-					<div class="form-group">
-					<input type="text" class="form-control" name="username"placeholder="Username">
-					<input type="password" class="form-control" name="password"placeholder="Password">
-					</div>
-					<button type="submit" class="btn btn-success">Login</button>
-					</form>
+					<a class="navbar-form navbar-right" style="font-size:25px;color:#fff;" href="home.php">نانوسوفت</a>
 				</div>
                 
             </div>
@@ -55,19 +60,8 @@
         <!-- /.container-fluid -->
     </nav>
 			<div class="container" style="margin:8% auto;">
-				<div class="col-sm-4 col-md-3">
-					<h2>Discuss with your friends</h2>
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-				</div>
-				 <div class="col-sm-5 col-md-4 pull-right">
-                   <div class="row">
-                   
-						<form method="POST" class="form-signin" action="functions/register.php">
+				<div class="col-sm-6 col-md-5">
+					<form method="POST" class="form-signin" action="functions/register.php">
 								<h3 class="text-center">Signup Here!</h3>
 							<input type="text" name="fname"placeholder="First Name"class="form-control" required>
 							<input type="text" name="lname"placeholder="Last Name"class="form-control" required>
@@ -80,6 +74,24 @@
 							<input type="password" placeholder="Password" name="password" class="form-control" required>
 							<input type="submit" value="Signup" class="btn btn-success" style="width:100%;">
 						</form>
+				</div>
+				 <div class="col-sm-5 col-md-4 pull-right">
+                   <div class="row">
+                   
+                       <h2>Discuss with your friends</h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.لا شكر فكلنا إخوة في الله
+نعملا شكر فكلنا إخوة في الله
+نعملا شكر فكلنا إخوة في الله
+نعملا شكر فكلنا إخوة في الله
+نعملا شكر فكلنا إخوة في الله
+نعم</p>
+                       
+						
 				</div>
 			</div>
 		</div>
